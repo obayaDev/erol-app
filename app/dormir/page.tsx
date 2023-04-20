9c207edd754fa7b6ae97406ed66634c82dd3b688
obayaDev <obayadev@gmail.com>
2023-04-20 13:16:02 +0200

    Update page.tsx

--- app/dormir/page.tsx
+++ app/dormir/page.tsx
@@ -1,4 +1,4 @@
-import { FaHandPointRight } from "react-icons/fa"
+import { FaHandPointRight, FaExclamation } from "react-icons/fa"
 import SleepForm from "@/components/sleep/form";
 
 export default function Dormir(){
@@ -13,37 +13,37 @@ export default function Dormir(){
             <SleepForm/>
           </div>
 
-          <div className="flex flex-col mt-5 lg:mt-0 px-8">
+          <div className="flex flex-col mt-5 lg:mt-0 px-8 max-w-lg">
             <div className="mt-3.5">
               <h1 className="text-blue-600 font-extrabold text-sm inline-block align-bottom uppercase lg:text-xs">Normativa per dormir al Cau i informacions varies</h1>
             </div>
             <div className="flex flex-col justify-star px-4 py-4 space-y-3">
               <div className="flex flex-row">
-                <FaHandPointRight/>
-                <div className="my-auto px-2"><p className="text-xs">Tip: els dies en color clar no s'hi pot dormir o ja estan ocupats.</p></div>
+                <FaExclamation className="my-auto w-7"/>
+                <div className="my-auto px-2 flex align-middle"><p className="text-xs font-semibold"><span className="font-extrabold text-base">Important!</span> Reviseu el correu i el telefon perquè si no està ben escrit no us podrem confirmar la reserva.</p></div>
               </div>
               <div className="flex flex-row">
-                <FaHandPointRight/>
+                <FaHandPointRight className="my-auto w-8 max-w-fit"/>
                 <div className="my-auto px-2"><p className="text-xs">Feu la reserva i rebreu un correu de confirmació amb un document que haureu d'omplir, firmar i enviar dos dies abans de l'estança.</p></div>
               </div>
               <div className="flex flex-row">
-                <FaHandPointRight/>
+                <FaHandPointRight className="my-auto w-8 max-w-fit"/>
                 <div className="my-auto px-2"><p className="text-xs">Els horaris d'entrada i sortida són de 8h a 20h.</p></div>
               </div>
               <div className="flex flex-row">
-                <FaHandPointRight/>
+                <FaHandPointRight className="my-auto w-8 max-w-fit"/>
                 <div className="my-auto px-2"><p className="text-xs">Máxim s'admeten a 8 caps i 30 infants</p></div>
               </div>
               <div className="flex flex-row">
-                <FaHandPointRight/>
+                <FaHandPointRight className="my-auto w-8 max-w-fit"/>
                 <div className="my-auto px-2"><p className="text-xs">Al enviar el formulari s'accepta les normes d'us de les instal·lacions especificades al formulari.</p></div>
               </div>
               <div className="flex flex-row">
-                <FaHandPointRight/>
+                <FaHandPointRight className="my-auto w-8 max-w-fit"/>
                 <div className="my-auto px-2"><p className="text-xs">En cas de desperfecte es pasarà la factura dels danys causats a l'escola pq quedi tal i com l'hem trobat.</p></div>
               </div>
               <div className="flex flex-row">
-                <FaHandPointRight/>
+                <FaHandPointRight className="my-auto w-8 max-w-fit"/>
                 <div className="my-auto px-2"><p className="text-xs">La página web està en procès de creació, l'apartat de reserves és totalment funcional però.</p></div>
               </div>
             </div>
