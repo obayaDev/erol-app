ec6bba6ec613c417cc6df3bb01faebc080711e6d
obayaDev <obayadev@gmail.com>
2023-04-24 04:44:51 +0200

    BitInt to Int Try

---
+++ prisma/migrations/20230424024354_big_int_to_int/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - The primary key for the `forms_sleep` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to alter the column `id` on the `forms_sleep` table. The data in that column could be lost. The data in that column will be cast from `Int` to `UnsignedInt`.
+
+*/
+-- AlterTable
+ALTER TABLE `forms_sleep` DROP PRIMARY KEY,
+    MODIFY `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    ADD PRIMARY KEY (`id`);
