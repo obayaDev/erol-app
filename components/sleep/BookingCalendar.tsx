b4d482a29252e32110f4a4d115d0e01053271277
obayaDev <obayadev@gmail.com>
2023-05-03 14:45:59 +0200

    Update BookingCalendar.tsx
    Se ha solucionado el problema que seleccionaba fechas que ya estaban ocupadas

--- components/sleep/BookingCalendar.tsx
+++ components/sleep/BookingCalendar.tsx
@@ -3,10 +3,11 @@
 import React, { useState, useEffect } from "react";
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
-import { DateRangePicker } from 'react-date-range';
+import { DateRange, DateRangePicker } from 'react-date-range';
 const rdrLocales = require('react-date-range/dist/locale');
 import { addDays, isWeekend, toDate } from 'date-fns';
 import LoadingDots from "@/components/login/loading-dots";
+import { start } from "repl";
 
 export default function BookingCalendar(props:{setDateIn: Function, setDateOut: Function}) {
 
@@ -36,50 +37,66 @@ export default function BookingCalendar(props:{setDateIn: Function, setDateOut: 
   const [disabledDates, setDisabledDates] = useState(calcWeekdays(new Date(), addDays(new Date(), 365)));
   const [enabledDates, setEnabledDates] = useState(calcWeekends(new Date(), addDays(new Date(), 365)));
 
-  useEffect(() => {
-    props.setDateIn(startDate);
-    props.setDateOut(endDate);
-    fetch("/api/sleep/getSleepForms")
-    .then((res) => res.json())
-    .then((data) => {
-      const datesToRemove:Date[] = [];
-      data.forEach((sleepForm: { dateIn: Date, dateOut: Date}) => {
-        sleepForm.dateIn = new Date(sleepForm.dateIn);
-        sleepForm.dateOut = new Date(sleepForm.dateOut);
-        sleepForm.dateIn.setHours(0, 0);
-        sleepForm.dateOut.setHours(0, 0);
-        datesToRemove.push(sleepForm.dateIn);
-        datesToRemove.push(sleepForm.dateOut);
-      });
+  let enDa:Date[]
 
-      disabledDates.push(...datesToRemove);
-      disabledDates.sort((a, b) => a.getTime() - b.getTime());
-      enabledDates.forEach((date) => {
-        date.setHours(0);
-        date.setMinutes(0);
-        date.setSeconds(0);
-        date.setMilliseconds(0);
-      });
-      const enDa = enabledDates.filter(date => !datesToRemove.some((date2 => date2.getTime() === date.getTime())));
-      setEnabledDates(enDa);
-      setStartDate(enabledDates[0]);
-      setEndDate(enabledDates[1]);
-      setLoading(false);
-    })
-  }, []);
-
-  const [startDate, setStartDate] = useState(enabledDates[0]);
-  const [endDate, setEndDate] = useState(enabledDates[1]);
-
-  let selectionRange = {
-    startDate: startDate,
-    endDate: endDate,
+  const [selectionRange, setSelectionRange] = useState({
+    startDate: new Date(),
+    endDate: addDays(new Date, 1),
     key: 'Selection'
-  };
+  });
+
+  const miPromesa = new Promise((resolve, reject) => {
+    useEffect(() => {
+      props.setDateIn(selectionRange.startDate);
+      props.setDateOut(selectionRange.endDate);
+      fetch("/api/sleep/getSleepForms")
+      .then((res) => res.json())
+      .then((data) => {
+        const datesToRemove:Date[] = [];
+        data.forEach((sleepForm: { dateIn: Date, dateOut: Date}) => {
+          sleepForm.dateIn = new Date(sleepForm.dateIn);
+          sleepForm.dateOut = new Date(sleepForm.dateOut);
+          sleepForm.dateIn.setHours(0, 0);
+          sleepForm.dateOut.setHours(0, 0);
+          datesToRemove.push(sleepForm.dateIn);
+          datesToRemove.push(sleepForm.dateOut);
+        });
+  
+        disabledDates.push(...datesToRemove);
+        disabledDates.sort((a, b) => a.getTime() - b.getTime());
+        enabledDates.forEach((date) => {
+          date.setHours(0);
+          date.setMinutes(0);
+          date.setSeconds(0);
+          date.setMilliseconds(0);
+        });
+        enDa = enabledDates.filter(date => !datesToRemove.some((date2 => date2.getTime() === date.getTime())));
+        setLoading(false);
+        resolve("ok");
+      })
+    }, []);
+  });
+  
+  miPromesa.then((result) => {
+    
+    setSelectionRange({
+      startDate: enDa[0],
+      endDate: enDa[1],
+      key: 'Selection'
+    });
+  });
+
+  useEffect(() => {
+      console.log('updated')
+      console.log(selectionRange)
+  }, [selectionRange])
 
   const handleSelect = (ranges:any,) =>{
-    setStartDate(ranges.Selection.startDate);
-    setEndDate(ranges.Selection.endDate);
+    setSelectionRange({
+      startDate: ranges.Selection.startDate,
+      endDate: ranges.Selection.endDate,
+      key: 'Selection'
+    });
     props.setDateIn(ranges.Selection.startDate);
     props.setDateOut(ranges.Selection.endDate);
   }
